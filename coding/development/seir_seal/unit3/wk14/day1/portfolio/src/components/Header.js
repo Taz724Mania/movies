33dc04314eb906ecc832e7e6b6de4ac8b6ade79f
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-01-24 00:34:33 -0500

    Header styling done

--- coding/development/seir_seal/unit3/wk14/day1/portfolio/src/components/Header.js
+++ coding/development/seir_seal/unit3/wk14/day1/portfolio/src/components/Header.js
@@ -1,14 +1,11 @@
 
-
-
 const Header = () => {
     return(
         <div className="Header">
-            The Header
+            <a href="https://github.com/Taz724Mania">Taz's Github</a>
+            <a href="https:www.linkedin.com/in/taz-campbell2724">Taz's LinkedIn</a>
         </div>
     )
 }
 
 export default Header
-
-
